8f039a3e7e3dd9dc46e94c25ea511bb53d4733cd
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2023-02-04 12:45:47 +0100

    Up

---
+++ Progetto#2/PlayerPosition.java
@@ -0,0 +1,17 @@
+/*
+ * Ene Alexandru, Frau Antonio
+ * 
+ * Data, nome file?
+ * 
+ * JavaSE-17
+ *
+ * ©
+ * 
+ * Descrizione programma 
+ */
+
+package BattagliaNavale;
+
+public interface PlayerPosition {
+	public void arrangeWatercrafts();
+}
